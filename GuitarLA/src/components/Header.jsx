c0ba220699e3f7b7f65892cef36cf43bb1d99ab7
Defiant7426 <pldelacruzv@gmail.com>
2024-08-08 10:34:01 -0500

    Agregando memo al carrito

--- GuitarLA/src/components/Header.jsx
+++ GuitarLA/src/components/Header.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import { React, useMemo } from 'react'
 
 export default function Header({ carrito }) {
-  const total = 100
+  // state derivado 
+  const isEmpty = useMemo( () => carrito.length === 0, [carrito]) //no se hace render de la aplicacion hasta que cambie el estado de carrito
+  // esto hace que "isEmpty" ya no sea una funcion, por lo que podemos llamarlo solo con "isEmpty"
+  // si no se quitan los parentisis puede que no funcione correctamente
+  const carritoTotal = useMemo(() => carrito.reduce((acc, item) => acc + item.price*item.cantidad, 0), [carrito])
 
   return (
     <header className="py-5 header">
@@ -25,54 +29,57 @@ export default function Header({ carrito }) {
               />
 
               <div id="carrito" className="bg-white p-3">
-                {carrito.length === 0 ? (
+                {isEmpty ? (
                   <p className="text-center">El carrito esta vacio</p>
                 ) : (
-                  <table className="w-100 table">
-                    <thead>
-                      <tr>
-                        <th>Imagen</th>
-                        <th>Nombre</th>
-                        <th>Precio</th>
-                        <th>Cantidad</th>
-                        <th></th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {carrito.map((guitar) => (
-                        <tr key={guitar.id}>
-                          <td>
-                            <img
-                              className="img-fluid"
-                              src={`/img/${guitar.image}.jpg`}
-                              alt="imagen guitarra"
-                            />
-                          </td>
-                          <td>{guitar.name}</td>
-                          <td className="fw-bold">${guitar.price}</td>
-                          <td className="flex align-items-start gap-4">
-                            <button type="button" className="btn btn-dark">
-                              -
-                            </button>
-                            {guitar.cantidad}
-                            <button type="button" className="btn btn-dark">
-                              +
-                            </button>
-                          </td>
-                          <td>
-                            <button className="btn btn-danger" type="button">
-                              X
-                            </button>
-                          </td>
+                  <>
+                    <table className="w-100 table">
+                      <thead>
+                        <tr>
+                          <th>Imagen</th>
+                          <th>Nombre</th>
+                          <th>Precio</th>
+                          <th>Cantidad</th>
+                          <th></th>
                         </tr>
-                      ))}
-                    </tbody>
-                  </table>
-                )}
+                      </thead>
+                      <tbody>
+                        {carrito.map((guitar) => (
+                          <tr key={guitar.id}>
+                            <td>
+                              <img
+                                className="img-fluid"
+                                src={`/img/${guitar.image}.jpg`}
+                                alt="imagen guitarra"
+                              />
+                            </td>
+                            <td>{guitar.name}</td>
+                            <td className="fw-bold">${guitar.price}</td>
+                            <td className="flex align-items-start gap-4">
+                              <button type="button" className="btn btn-dark">
+                                -
+                              </button>
+                              {guitar.cantidad}
+                              <button type="button" className="btn btn-dark">
+                                +
+                              </button>
+                            </td>
+                            <td>
+                              <button className="btn btn-danger" type="button">
+                                X
+                              </button>
+                            </td>
+                          </tr>
+                        ))}
+                      </tbody>
+                    </table>
 
-                <p className="text-end">
-                  Total pagar: <span className="fw-bold">$899</span>
-                </p>
+                    <p className="text-end">
+                      Total pagar:
+                      <span className="fw-bold">${carritoTotal}</span>
+                    </p>
+                  </>
+                )}
                 <button className="btn btn-dark w-100 mt-3 p-2">
                   Vaciar Carrito
                 </button>
