852d6c6164c1cb808b7855993e7b116b7ad0af3b
Defiant7426 <pldelacruzv@gmail.com>
2024-08-13 15:02:48 -0500

    exportando la logica de App.jsx

--- GuitarLA/src/App.jsx
+++ GuitarLA/src/App.jsx
@@ -1,76 +1,18 @@
-import { useEffect, useState } from "react"
+
 import Header from "./components/Header"
 import Guitar from "./components/Guitar"
-import { db }  from "./data/db"
 import useCarrito from "./components/hooks/useCarrito"
 
 function App() {
 
-  const { auth, arreglo } = useCarrito()
-
-  console.log(auth)
-  console.log(arreglo)
-
-  const initialCarrito = () => {
-    const localStorageCarrito = localStorage.getItem("carrito")
-    return localStorageCarrito ? JSON.parse(localStorageCarrito) : []
-  }
-
-  const [data, setData] = useState(db)
-  const [carrito, setCarrito] = useState(initialCarrito)
-
-  const MIN_ITEMS = 0
-  const MAX_ITEMS = 5
-
-  useEffect(() => {
-    localStorage.setItem("carrito", JSON.stringify(carrito))
-  }, [carrito])
-
-  function addToCard(item){
-    const itemExist = carrito.findIndex((guitar)  => guitar.id === item.id) // -1 si no existe el item en el carrito
-    if(itemExist >= 0){ // si el item existe en el carrito
-      addOneUnitToCarrito(item.id) // agrego una unidad al item
-    }
-    else{
-      setCarrito([...carrito, {...item, cantidad: 1}]) // agrego el item al carrito con cantidad 1
-    }
-
-  } 
-
-  function removeFromCarrito(id) {
-    const newCarrito = carrito.filter((guitar) => guitar.id !== id) // filtro el carrito para que no contenga el item que quiero eliminar
-    setCarrito(newCarrito) // actualizo el carrito
-  }
-
-  function restOneUnitFromCarrito(id){
-    const newCarrito = carrito.map((guitar) => {
-      if(guitar.id === id){
-        guitar.cantidad--
-        return guitar
-      } else{
-        return guitar
-      }
-    }).filter((guitar) => guitar.cantidad > MIN_ITEMS)
-    setCarrito(newCarrito)
-  }
-
-  function addOneUnitToCarrito(id){
-    const newCarrito = carrito.map((guitar) => {
-      if(guitar.id === id && guitar.cantidad < MAX_ITEMS){
-        guitar.cantidad++
-        return guitar
-      } else{
-        return guitar
-      }
-    })
-    setCarrito(newCarrito)
-  }
-
-  function clearCarrito(carrito){
-    setCarrito([])
-  }
-
-
+  const { 
+    data, 
+    carrito,
+    addToCard,
+    removeFromCarrito,
+    clearCarrito,
+    addOneUnitToCarrito,
+    restOneUnitFromCarrito } = useCarrito()
 
   return (
     <>
@@ -91,7 +33,6 @@ function App() {
             <Guitar 
               key={guitar.id} 
               guitar={guitar} 
-              setCarrito={setCarrito}
               addToCard={addToCard}
             />
           ))}
--- GuitarLA/src/components/hooks/useCarrito.js
+++ GuitarLA/src/components/hooks/useCarrito.js
@@ -1,10 +1,76 @@
+import { useEffect, useState } from "react"
+import { db } from "../../data/db.js"
+
 export default function useCarrito() {
-  const auth = "disponible"
-  const arreglo = ["1", "2"]
+  
+  const initialCarrito = () => {
+    const localStorageCarrito = localStorage.getItem("carrito")
+    return localStorageCarrito ? JSON.parse(localStorageCarrito) : []
+  }
+
+  const [data] = useState(db)
+  const [carrito, setCarrito] = useState(initialCarrito)
+
+  const MIN_ITEMS = 0
+  const MAX_ITEMS = 5
+
+  useEffect(() => {
+    localStorage.setItem("carrito", JSON.stringify(carrito))
+  }, [carrito])
+
+  function addToCard(item){
+    const itemExist = carrito.findIndex((guitar)  => guitar.id === item.id) // -1 si no existe el item en el carrito
+    if(itemExist >= 0){ // si el item existe en el carrito
+      addOneUnitToCarrito(item.id) // agrego una unidad al item
+    }
+    else{
+      setCarrito([...carrito, {...item, cantidad: 1}]) // agrego el item al carrito con cantidad 1
+    }
+
+  } 
+
+  function removeFromCarrito(id) {
+    const newCarrito = carrito.filter((guitar) => guitar.id !== id) // filtro el carrito para que no contenga el item que quiero eliminar
+    setCarrito(newCarrito) // actualizo el carrito
+  }
+
+  function restOneUnitFromCarrito(id){
+    const newCarrito = carrito.map((guitar) => {
+      if(guitar.id === id){
+        guitar.cantidad--
+        return guitar
+      } else{
+        return guitar
+      }
+    }).filter((guitar) => guitar.cantidad > MIN_ITEMS)
+    setCarrito(newCarrito)
+  }
+
+  function addOneUnitToCarrito(id){
+    const newCarrito = carrito.map((guitar) => {
+      if(guitar.id === id && guitar.cantidad < MAX_ITEMS){
+        guitar.cantidad++
+        return guitar
+      } else{
+        return guitar
+      }
+    })
+    setCarrito(newCarrito)
+  }
+
+  function clearCarrito(carrito){
+    setCarrito([])
+  }
+
 
   return{
-    auth,
-    arreglo
+    data, 
+    carrito,
+    addToCard,
+    removeFromCarrito,
+    clearCarrito,
+    addOneUnitToCarrito,
+    restOneUnitFromCarrito
   }
 }
 
