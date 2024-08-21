f20506c6a9ce65c6a65410bcfc575c87e5b40a81
Defiant7426 <pldelacruzv@gmail.com>
2024-08-21 17:19:00 -0500

    Evitando duplicados

--- calculadora-propinas/src/hooks/useOrder.ts
+++ calculadora-propinas/src/hooks/useOrder.ts
@@ -6,11 +6,29 @@ export default function useOrder() {
     const [order, setOrder] = useState<OrderItem[]>([])
 
     const addItem = (item: MenuItem) => {
-        const newItem : OrderItem = {
+
+        const itemExist = order.find((orderItem) => orderItem.id === item.id)
+
+        if (itemExist) {
+            const updatedOrder = order.map((orderItem) => {
+                if (orderItem.id === item.id) {
+                    return {
+                        ...orderItem, // Copia todas las propiedades del objeto
+                        cantidad: orderItem.cantidad + 1 // Actualiza la cantidad
+                    }
+                }
+                return orderItem
+            })
+            setOrder(updatedOrder)
+        }else{
+          const newItem : OrderItem = {
             ...item,
             cantidad: 1
+          }
+          setOrder([...order, newItem])
         }
-        setOrder([...order, newItem])
+
+        
     }
 
     console.log(order)
