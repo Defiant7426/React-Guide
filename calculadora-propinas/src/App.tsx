b3850dedd43d8905e53a9ae3584f4c3dc1012fae
Defiant7426 <pldelacruzv@gmail.com>
2024-08-22 23:13:34 -0500

    Agregando el subtotalAmount en OrderTotal

--- calculadora-propinas/src/App.tsx
+++ calculadora-propinas/src/App.tsx
@@ -33,7 +33,7 @@ function App() {
           />
 
           <OrderTotal 
-          
+            order={order}
           />
         </div>
       </main>
--- calculadora-propinas/src/components/OrderTotal.tsx
+++ calculadora-propinas/src/components/OrderTotal.tsx
@@ -1,6 +1,21 @@
+import { useMemo } from "react";
+import { OrderItem } from "../types";
+import { formatCurrency } from "../helpers";
 
+type OrderTotalProps = {
 
-export default function OrderTotal() {
+        order: OrderItem[];
+
+}
+
+export default function OrderTotal({order}: OrderTotalProps) {
+
+    const subtotalAmount = useMemo(() => {
+        return order.reduce((acc, item) => {
+            return acc + (item.price * item.cantidad)
+        }, 0)
+    }, [order])
+
   return (
     <>
     
@@ -8,15 +23,15 @@ export default function OrderTotal() {
 
             <h2 className="font-black text-2xl">Totales y propinas</h2>
             <p>Subtotal a pagar: {' '}
-                <span className="font-black text-lg"> $0</span>
+                <span className="font-black text-lg"> {formatCurrency(subtotalAmount)}</span>
             </p>
 
             <p>Propina sugerida del 10%: {' '}
-                <span className="font-black text-lg"> $0</span>
+                <span className="font-black text-lg"> {formatCurrency(subtotalAmount*0.1)}</span>
             </p>
 
             <p>Total a Pagar: {' '}
-                <span className="font-black text-lg"> $0</span>
+                <span className="font-black text-lg"> {formatCurrency(subtotalAmount*1.1)}</span>
             </p>
 
         </div>
