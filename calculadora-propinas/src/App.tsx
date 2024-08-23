bb39543ca6807aba7642d1e6feba2ea748739177
Defiant7426 <pldelacruzv@gmail.com>
2024-08-22 22:53:08 -0500

    Agregando subtotal, propina y total a pagar

--- calculadora-propinas/src/App.tsx
+++ calculadora-propinas/src/App.tsx
@@ -1,43 +1,40 @@
-import MenuItems from "./components/MenuItems"
-import OrderContents from "./components/OrderContents"
-import { menuItems } from "./data/db"
-import useOrder from "./hooks/useOrder"
+import MenuItems from './components/MenuItems'
+import OrderContents from './components/OrderContents'
+import { menuItems } from './data/db'
+import useOrder from './hooks/useOrder'
+import OrderTotal from './components/OrderTotal'
 
 function App() {
-  
   const { order, addItem, removeItem } = useOrder()
 
   return (
     <>
       <header className="bg-teal-400 py-5">
-        <h1 className="text-center text-4xl font-black">Calculadora de propinas</h1>
+        <h1 className="text-center text-4xl font-black">
+          Calculadora de propinas
+        </h1>
       </header>
 
       <main className="max-w-7xl mx-auto py-20 grid md:grid-cols-2 bg-slate-200 px-5">
         <div className="p-5">
-
           <h2 className="text-4xl font-black ">Menu</h2>
-          
-          <div className="space-y-3 mt-10">
-          {menuItems.map((item) => (
-            <MenuItems
-              key={item.id}
-              item = {item}
-              addItem={addItem}
-            />
-          ))}
-          </div>
 
-          
+          <div className="space-y-3 mt-10">
+            {menuItems.map((item) => (
+              <MenuItems key={item.id} item={item} addItem={addItem} />
+            ))}
+          </div>
         </div>
 
         <div className="border border-dashed border-slate-300 p-5 rounded-lg space-y-10">
+          <OrderContents 
+            order={order} 
+            removeItem={removeItem} 
+          />
 
-            <OrderContents 
-              order={order}
-              removeItem={removeItem}
-            />
-
+          <OrderTotal 
+          
+          />
         </div>
       </main>
     </>
---
+++ calculadora-propinas/src/components/OrderTotal.tsx
@@ -0,0 +1,31 @@
+
+
+export default function OrderTotal() {
+  return (
+    <>
+    
+        < div className="space-y-3">
+
+            <h2 className="font-black text-2xl">Totales y propinas</h2>
+            <p>Subtotal a pagar: {' '}
+                <span className="font-black text-lg"> $0</span>
+            </p>
+
+            <p>Propina sugerida del 10%: {' '}
+                <span className="font-black text-lg"> $0</span>
+            </p>
+
+            <p>Total a Pagar: {' '}
+                <span className="font-black text-lg"> $0</span>
+            </p>
+
+        </div>
+
+        <button>
+
+
+        </button>
+    
+    </>
+  )
+}
