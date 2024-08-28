3f880b46b86589c978fffede21fc82acaf550108
Defiant7426 <pldelacruzv@gmail.com>
2024-08-28 15:36:26 -0500

    chore: Update Form component with category select dropdown

--- calorie-tracker/src/components/Form.tsx
+++ calorie-tracker/src/components/Form.tsx
@@ -1,16 +1,22 @@
+import { cotegories } from "../data/categories"
+
 export default function Form() {
   return (
-    <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-        <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">
-            Nombre
+    <form className="space-y-5 bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <div className="grid grid-cols-1 gap-3 mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="category">
+            Calorías
             </label>
             <select
             className="shadow appearance-none border border-slate-300 bg-white rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="name"
-            type="text"
-            placeholder="Nombre"
-            select/>
+            id="category"
+            >
+            {cotegories.map((category) => (
+                <option key={category.id} value={category.id}>
+                {category.name}
+                </option>
+            ))}
+            <select/>
         </div>
         <div className="mb-6">
             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="calories">
---
+++ calorie-tracker/src/data/categories.ts
@@ -0,0 +1,6 @@
+import type { Category } from '../types';
+
+export const cotegories = [
+    { id: 1, name: 'Comida'},
+    { id: 2, name: 'Ejercicios'},
+]
