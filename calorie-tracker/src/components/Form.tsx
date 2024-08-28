31f4ab0da25c2fd83837243142674cbdfb1eb61e
Defiant7426 <pldelacruzv@gmail.com>
2024-08-27 23:18:39 -0500

    chore: Update Form component with input fields and submit button

--- calorie-tracker/src/components/Form.tsx
+++ calorie-tracker/src/components/Form.tsx
@@ -1,5 +1,36 @@
 export default function Form() {
   return (
-    <div>Form</div>
+    <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">
+            Nombre
+            </label>
+            <select
+            className="shadow appearance-none border border-slate-300 bg-white rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="name"
+            type="text"
+            placeholder="Nombre"
+            select/>
+        </div>
+        <div className="mb-6">
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="calories">
+            Calorias
+            </label>
+            <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="calories"
+            type="number"
+            placeholder="Calorias"
+            />
+        </div>
+        <div className="flex items-center justify-between">
+            <button
+            className="bg-lime-600 hover:bg-lime-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="button"
+            >
+            Agregar
+            </button>
+        </div>
+    </form>
   )
 }
