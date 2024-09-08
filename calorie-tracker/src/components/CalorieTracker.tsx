c07e803cb731f594ccda0dca4256ce8c5ccf37bd
Defiant7426 <pldelacruzv@gmail.com>
2024-09-07 21:46:05 -0500

    Mostrando las calorias consumidas

--- calorie-tracker/src/components/CalorieTracker.tsx
+++ calorie-tracker/src/components/CalorieTracker.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from "react"
 import { Activity } from "../types"
 
 type CalorieTrackerProp = {
@@ -5,7 +6,21 @@ type CalorieTrackerProp = {
     }
 
 export default function CalorieTracker({activities}: CalorieTrackerProp) {
+
+    //contadores
+    const totalCalories = useMemo(() => activities.reduce((acc, activity) => acc + activity.calories, 0), [activities])
+
   return (
-    <div>CalorieTracker</div>
+    <>
+    
+    <h2 className="text-4xl font-bold text-white text-center my-5">Resumen de calorias</h2>
+
+    <div className="flex flex-col items-center md:flex-row md:justify-between gap-5 mt-10 ">
+        <p className="text-white font-bold rounded-full grid grid-cols-1 gap-3 text-center">
+            <span className="font-black text-6xl text-orange-400">{totalCalories}</span>
+            Consumidas</p>
+    </div>
+    
+    </>
   )
 }
